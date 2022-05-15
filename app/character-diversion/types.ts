94721c215fcb12f6733f7a0e04cc19e27a90eee6
Leon <leonfvogler@gmail.com>
2022-05-15 16:38:37 +0200

    update types

--- app/character-diversion/types.ts
+++ app/character-diversion/types.ts
@@ -1,53 +1,93 @@
-import type { SamsaFont, SamsaGlyph } from "~~/assets/samsa-core";
+import { SamsaFont, SamsaGlyph } from '~~/assets/samsa-core'
 
 export interface Discourse {
-	id: string;
-	attributes: {
-		name: string;
-		title: string;
-		description: string;
-		author: string;
-		createdAt: Date;
-		updatedAt: Date;
-		opinions: {
-			data: Opinion[];
-		};
-		fonts: SamsaFont[];
-	}
-}
-export interface Comment {
-	id: string;
-	name: string;
-	author: string;
-	createdAt: Date;
-	updatedAt: Date;
+  id: string
+  attributes: {
+    title: string
+    content: string
+    author?: string
+    createdAt?: Date
+    updatedAt?: Date
+    opinions?: Opinion[]
+    fonts: SamsaFont[]
+  }
 }
 export interface Opinion {
-	id: string;
-	attributes: {
-		title: string;
-		content: string;
-		author: string;
-		createdAt: Date;
-		updatedAt: Date;
-		comments?: Comment[];
-		fonts?: SamsaFont[];
-		glyphs?: SamsaGlyph[];
-		tuple?: number[];
-		spectrum?: FontSpectrum[];
-	}
+  id: string
+  attributes: {
+    title?: string
+    content?: string
+    author?: string
+    createdAt?: Date
+    updatedAt?: Date
+    comments?: Comment[]
+    fontSpectrum?: FontSpectrum[]
+    glyphSpectrum?: Array<GlyphSpectrum | string>
+    tupleSpectrum?: TupleSpectrum[]
+  }
+}
+export interface Comment {
+  id: string
+  name: string
+  author: string
+  createdAt: Date
+  updatedAt: Date
 }
 export interface FontSpectrum {
-	font?: SamsaFont;
-	glyphSpectrum?: GlyphSpectrum[];
-	tupleSpectrum?: TupleSpectrum[];
+  font: SamsaFont
+  glyphSpectrum?: Array<GlyphSpectrum | string>
+  tupleSpectrum?: TupleSpectrum[]
 }
 export interface GlyphSpectrum {
-	glyphs?: string[];
-	tupleSpectrum?: TupleSpectrum[];
+  glyph: string
+  samsaGlyph: SamsaGlyph
+  tupleSpectrum?: TupleSpectrum[]
 }
 export interface TupleSpectrum {
-	name: string;
-	min: number[];
-	max: number[];
+  name: string
+  min: number
+  max: number
 }
+/**
+const discourse: Discourse = {
+  id: '2341',
+  attributes: {
+    title: 'new discourse',
+    content: 'new discourse',
+    fonts: [new SamsaFont()],
+    opinions: [
+      {
+        id: '2418',
+        attributes: {
+          title: 'i disagree',
+          spectrum: [
+            {
+              font: new SamsaFont(),
+              tupleSpectrum: [
+                {
+                  name: 'wdth',
+                  min: 0,
+                  max: 0.3,
+                },
+              ],
+              glyphSpectrum: [
+                'a',
+                {
+                  glyph: 'b',
+                  tupleSpectrum: [
+                    {
+                      name: 'wdth',
+                      min: 0,
+                      max: 0.6,
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      },
+    ],
+  },
+}
+ */
