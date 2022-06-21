0a7bd7df05f01be1b65c1cb682347f2ba2a1ff48
Leon Vogler <leonfvogler@gmail.com>
2022-06-21 18:46:42 +0200

    update

--- app/character-diversion-backend/config/database.js
+++ app/character-diversion-backend/config/database.js
@@ -1,20 +1,19 @@
-const { parse } = require("pg-connection-string");
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client: 'postgres',
-      connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
-        ssl: { rejectUnauthorized: false },
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
       },
-      debug: false,
     },
-  }
-};
+    debug: false,
+  },
+});
--- app/character-diversion-backend/config/server.js
+++ app/character-diversion-backend/config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  proxy: true,
+  url: env('MY_HEROKU_URL'),
+  app: { 
+    keys: env.array('APP_KEYS')
   },
-});
+})
