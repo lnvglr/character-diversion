1ad34da613d98249edb3246c9817fbe204fdb351
Leon Vogler <leonfvogler@gmail.com>
2022-06-22 14:38:26 +0200

    error handling

--- app/character-diversion/components/NavBar.vue
+++ app/character-diversion/components/NavBar.vue
@@ -6,36 +6,36 @@
     <ul
       class="flex flex-row sm:flex-col justify-around items-center p-2 sm:max-w-2xl sm:h-full mx-auto gap-5"
     >
-      <li
-        v-for="(route, index) in routes"
-        :key="route.name"
-        :class="{ 'mb-auto': index === routes.length - 1 }"
-      >
-        <NuxtLink
-          v-if="route.icon"
-          :to="route.path"
-          class="flex flex-col items-center justify-center rounded-md duration-100 border-beige-200 hover:bg-beige-100 active:scale-95 active:text-primary-600 w-16 h-16 md:w-16 md:h-16 dark:text-slate-200 dark:hover:bg-beige-800"
+      <ClientOnly>
+        <li
+          v-for="(route, index) in routes"
+          :key="route.name"
+          :class="{ 'mb-auto': index === routes.length - 1 }"
         >
-          <ClientOnly>
+          <NuxtLink
+            v-if="route.icon"
+            :to="route.path"
+            class="flex flex-col items-center justify-center rounded-md duration-100 border-beige-200 hover:bg-beige-100 active:scale-95 active:text-primary-600 w-16 h-16 md:w-16 md:h-16 dark:text-slate-200 dark:hover:bg-beige-800"
+          >
             <Icon :name="route.icon" class="fa-lg" />
             <span class="text-xs mt-2" v-html="route.name" />
-          </ClientOnly>
-        </NuxtLink>
-      </li>
-      <Button
-        v-if="!$strapi.user"
-        to="/login"
-        class="hidden sm:block"
-        icon="arrow-right-to-bracket"
-        :label="false"
-      />
-      <ButtonLogout
-        v-else
-        class="clear hidden sm:block"
-        color="alert"
-        :icon="true"
-        :label="false"
-      />
+          </NuxtLink>
+        </li>
+        <Button
+          v-if="!$strapi.user"
+          to="/login"
+          class="hidden sm:block"
+          icon="arrow-right-to-bracket"
+          :label="false"
+        />
+        <ButtonLogout
+          v-else
+          class="clear hidden sm:block"
+          color="alert"
+          :icon="true"
+          :label="false"
+        />
+      </ClientOnly>
     </ul>
   </Card>
 </template>
