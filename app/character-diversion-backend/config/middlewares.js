c677d82ae2f18df492ece0536bd073cbe17ee559
Leon Vogler <leonfvogler@gmail.com>
2022-06-26 11:33:16 +0200

    add aws

--- app/character-diversion-backend/config/middlewares.js
+++ app/character-diversion-backend/config/middlewares.js
@@ -1,6 +1,5 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
@@ -9,4 +8,30 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'character-diversion.s3.eu-central-1.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'character-diversion.s3.eu-central-1.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
--- app/character-diversion-backend/config/plugins.js
+++ app/character-diversion-backend/config/plugins.js
@@ -21,5 +21,21 @@ module.exports = ({ env }) => ({
   },
   upload: {
     enabled: true,
-  },
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET'),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  }
 });
