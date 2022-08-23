720796514e0b1dbf87b7889c62ed85bb7a8e7a92
Leon Vogler <leonfvogler@gmail.com>
2022-08-23 13:48:18 +0200

    error handling

--- app/character-diversion/components/Author.vue
+++ app/character-diversion/components/Author.vue
@@ -1,54 +1,62 @@
 <template>
-	<span class="flex gap-2 items-center text-beige-400" v-if="author">
-		<Image class="object-cover rounded-full border border-beige-300" :class="`w-${imageSize} h-${imageSize}`" :style="{minWidth: Number(imageSize) / 4 + 'rem'}"
-			:src="avatar" size="thumbnail" :placeholder="name[0]" />
-		<span
-			v-if="info"
-			v-html="[`<b>${name}</b>`, publishedAt].filter(e => e).join(' · ')"></span>
-	</span>
+  <span class="flex gap-2 items-center text-beige-400" v-if="author">
+    <Image
+      class="object-cover rounded-full border border-beige-300"
+      :class="`w-${imageSize} h-${imageSize}`"
+      :style="{ minWidth: Number(imageSize) / 4 + 'rem' }"
+      :src="avatar"
+      size="thumbnail"
+      :placeholder="name[0]"
+    />
+    <span
+      v-if="info"
+      v-html="[`<b>${name}</b>`, publishedAt].filter((e) => e).join(' · ')"
+    ></span>
+  </span>
 </template>
 <script lang="ts">
-import { Author, Opinion, Discourse, User } from "~/types"
+import { Author, Opinion, Discourse, User } from "~/types";
 
 export default defineComponent({
-	props: {
-		post: {
-			type: Object as () => Discourse | Opinion
-		},
-		user: {
-			type: Object as () => User
-		},
-		imageSize: {
-			type: String,
-			default: '8'
-		},
-		info: {
-			type: Boolean,
-			default: true
-		},
-	},
-	mounted() {
-		if (!this.avatar || !this.author) {
-		return this.$strapi
-			.findOne("users", this.author.id, { populate: ["avatar"] })
-			.then(({avatar}) => {
-				this.author.avatar = avatar;
-			});
-		}
-	},
-	computed: {
-		author(): Author {
-			return this.post?.attributes.author?.data || this.user
-		},
-		avatar() {
-			return this.author?.attributes?.avatar?.data?.attributes || this.author?.avatar
-		},
-		name() {
-			return this.author?.attributes?.name || this.author?.name
-		},
-		publishedAt() {
-			return this.$f.utils.relativeTime(this.post?.attributes.publishedAt)
-		},
-	}
-})
+  props: {
+    post: {
+      type: Object as () => Discourse | Opinion,
+    },
+    user: {
+      type: Object as () => User,
+    },
+    imageSize: {
+      type: String,
+      default: "8",
+    },
+    info: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  mounted() {
+    if (!this.avatar || !this.author) {
+      return this.$strapi
+        .findOne("users", this.author?.id, { populate: ["avatar"] })
+        .then(({ avatar }) => {
+          this.author.avatar = avatar;
+        });
+    }
+  },
+  computed: {
+    author(): Author {
+      console.log(this.post);
+      return this.post?.attributes.author?.data || this.user;
+    },
+    avatar() {
+      return this.author?.attributes?.avatar?.data?.attributes || this.author?.avatar;
+    },
+    name() {
+      return this.author?.attributes?.name || this.author?.name;
+    },
+    publishedAt() {
+      return this.$f.utils.relativeTime(this.post?.attributes.publishedAt);
+    },
+  },
+});
 </script>
--- app/character-diversion/components/DiscourseCard.vue
+++ app/character-diversion/components/DiscourseCard.vue
@@ -1,32 +1,48 @@
 <template>
-  <Card class="min-h-[200px] grid duration-75 hover:rounded-md hover:shadow-[inset_0_0_0_2px_var(--color-beige-300)] dark:hover:shadow-[inset_0_0_0_2px_var(--color-neutral-900)]" v-if="discourse">
+  <Card
+    class="min-h-[200px] grid duration-75 hover:rounded-md hover:shadow-[inset_0_0_0_2px_var(--color-beige-300)] dark:hover:shadow-[inset_0_0_0_2px_var(--color-neutral-900)]"
+    v-if="discourse"
+  >
     <NuxtLink
       :to="`/discourse/${discourse.id}`"
       class="p-4 flex flex-col flex-1 items-stretch"
     >
-    <div class="flex items-center gap-3 mb-5">
-      <div class="text-5xl text-info-500 bg-beige-100 flex items-center justify-center p-2 rounded-md w-20 h-20 min-w-[5rem] overflow-hidden" v-html="fontPreview"></div>
+      <div class="flex items-center gap-3 mb-5">
+        <div
+          class="text-5xl text-info-500 bg-beige-100 flex items-center justify-center p-2 rounded-md w-20 h-20 min-w-[5rem] overflow-hidden"
+          v-html="fontPreview"
+        ></div>
         <h2 class="text-2xl font-bold leading-tight flex justify-between">
-          {{ discourse.attributes.title
-          }}
+          {{ discourse.attributes.title }}
         </h2>
       </div>
       <div class="mt-auto py-2">
-      <span class="text-xs flex gap-2">
-        <span class="flex gap-1">{{$t('opinion', opinions.length)}}<UITag :round="true">{{opinions.length}}</UITag></span>
-        <span class="flex gap-1">{{$t('participant', participants.length)}}<UITag :round="true">{{participants.length}}</UITag></span>
-      </span>
+        <span class="text-xs flex gap-2">
+          <span class="flex gap-1"
+            >{{ $t("opinion", opinions.length)
+            }}<UITag :round="true">{{ opinions.length }}</UITag></span
+          >
+          <span class="flex gap-1"
+            >{{ $t("participant", participants.length)
+            }}<UITag :round="true">{{ participants.length }}</UITag></span
+          >
+        </span>
       </div>
-      <div class="flex justify-between items-center border-t border-beige-200 dark:border-neutral-600 pt-4">
-      <Author :post="discourse" class="text-sm" /><Button
-            v-if="discourse.attributes.author?.data?.id === $strapi.user?.id"
-            @click.prevent="removeDiscourse(discourse.id)"
-            color="alert"
-            class="sm text-sm clear"
-            icon="trash"
-            label="false"
-          />
-          </div>
+      <div
+        class="flex justify-between items-center border-t border-beige-200 dark:border-neutral-600 pt-4"
+      >
+        <Author :post="discourse" class="text-sm" />
+        <Button
+          v-if="
+            $strapi.user?.id && discourse.attributes.author?.data?.id === $strapi.user?.id
+          "
+          @click.prevent="removeDiscourse(discourse.id)"
+          color="alert"
+          class="sm text-sm clear"
+          icon="trash"
+          label="false"
+        />
+      </div>
     </NuxtLink>
   </Card>
 </template>
@@ -45,7 +61,7 @@ export default defineComponent({
       return {
         url: this.discourse?.attributes.font.data[0].attributes.url,
         name: this.discourse?.attributes.font.data[0].attributes.name,
-      }
+      };
     },
     previewGlyphs() {
       const font = this.font.name;
@@ -76,7 +92,13 @@ export default defineComponent({
       return this.discourse?.attributes.opinions?.data;
     },
     participants() {
-      return [...new Set(this.discourse?.attributes.opinions?.data.map((opinion: Opinion) => opinion.attributes.author?.data?.id))]
+      return [
+        ...new Set(
+          this.discourse?.attributes.opinions?.data.map(
+            (opinion: Opinion) => opinion.attributes.author?.data?.id
+          )
+        ),
+      ];
     },
   },
   methods: {
@@ -86,6 +108,6 @@ export default defineComponent({
         .then(({ data }: Strapi4Response<Discourse>) => delete discourse.all[data.id]);
     },
   },
-})
+});
 </script>
 <style></style>
